87a008f9b742cd9f70d89e276d311a8184423f76
HanleiRayWang <raywang0703@gmail.com>
2017-11-08 21:02:05 -0800

    Create _253Meeting_Rooms_II.java

---
+++ _253Meeting_Rooms_II.java
@@ -0,0 +1,57 @@
+/*
+
+
+Given an array of meeting time intervals consisting of start and end times [[s1,e1],[s2,e2],...] (si < ei), 
+find the minimum number of conference rooms required.
+
+For example,
+Given [[0, 30],[5, 10],[15, 20]],
+return 2.
+
+
+*/
+
+
+/**
+ * Definition for an interval.
+ * public class Interval {
+ *     int start;
+ *     int end;
+ *     Interval() { start = 0; end = 0; }
+ *     Interval(int s, int e) { start = s; end = e; }
+ * }
+ */
+ 
+ /*
+ 
+ we acknowledge three facts:
+1.  The numbers of the intervals give chronological orders
+2.  When an ending event occurs, there must be a starting event has happened before that, 
+    where “happen before” is defined by the chronological orders given by the intervals
+3.  Meetings that started which haven’t ended yet have to be put into different meeting rooms, 
+    and the number of rooms needed is the number of such meetings
+    
+for example, we have meetings that span along time as follows:
+
+|_____|
+      |______|
+|________|
+        |_______|
+Then, the start time array and end time array after sorting appear like follows:
+
+||    ||
+     |   |   |  |
+     
+Initially, endsItr points to the first end event, and we move i which is the start event pointer. 
+As we examine the start events, we’ll find the first two start events happen before the end event that endsItr points to, 
+so we need two rooms (we magically created two rooms), as shown by the variable rooms. 
+Then, as i points to the third start event, we’ll find that this event happens after the end event pointed by endsItr, 
+then we increment endsItr so that it points to the next end event. 
+What happens here can be thought of as one of the two previous meetings ended, 
+and we moved the newly started meeting into that vacant room, thus we don’t need to increment rooms at this time 
+and move both of the pointers forward.
+Next, because endsItr moves to the next end event, we’ll find that the start event pointed 
+by i happens before the end event pointed by endsItr. 
+Thus, now we have 4 meetings started but only one ended, so we need one more room. And it goes on as this.
+ 
+ */
