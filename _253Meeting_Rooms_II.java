fc89af5dd41baa69f55b9a9a83577814614c936d
HanleiRayWang <raywang0703@gmail.com>
2017-11-08 22:53:04 -0800

    Update _253Meeting_Rooms_II.java

--- _253Meeting_Rooms_II.java
+++ _253Meeting_Rooms_II.java
@@ -22,6 +22,56 @@ return 2.
  * }
  */
  
+
+
+
+
+//正常的解法, 使用PriorityQueue/Min_Heap
+class Solution {
+    public int minMeetingRooms(Interval[] intervals) {
+        if (intervals == null || intervals.length == 0)
+            return 0;
+
+        // Sort the intervals by start time
+        Arrays.sort(intervals, new Comparator<Interval>() {
+            public int compare(Interval a, Interval b) { return a.start - b.start; }
+        });
+
+        // Use a min heap to track the minimum end time of merged intervals
+        PriorityQueue<Interval> heap = new PriorityQueue<Interval>(intervals.length, new Comparator<Interval>() {
+            public int compare(Interval a, Interval b) { return a.end - b.end; }
+        });
+
+        // start with the first meeting, put it to a meeting room
+        heap.offer(intervals[0]);
+
+        for (int i = 1; i < intervals.length; i++) {
+            // get the meeting room that finishes earliest
+            Interval interval = heap.poll();
+
+            if (intervals[i].start >= interval.end) {
+                // if the current meeting starts right after 
+                // there's no need for a new room, merge the interval
+                interval.end = intervals[i].end;
+            } else {
+                // otherwise, this meeting needs a new room
+                heap.offer(intervals[i]);
+            }
+
+            // don't forget to put the meeting room back
+            heap.offer(interval);
+        }
+
+        return heap.size();
+    }
+}
+
+
+
+
+
+
+
  /*
  
  we acknowledge three facts:
@@ -55,3 +105,29 @@ by i happens before the end event pointed by endsItr.
 Thus, now we have 4 meetings started but only one ended, so we need one more room. And it goes on as this.
  
  */
+
+
+//fancy的解法
+class Solution {
+    public int minMeetingRooms(Interval[] intervals) {
+        int len = intervals.length;
+        int[] starts = new int[len];
+        int[] ends = new int[len];
+        for(int i=0; i<len; i++) {
+            starts[i] = intervals[i].start;
+            ends[i] = intervals[i].end;
+        }
+        Arrays.sort(starts);
+        Arrays.sort(ends);
+        
+        int room=0;
+        int endItr=0;
+        for(int i=0;i<len;i++){
+            if(starts[i]<ends[endItr])
+                room++;
+            else
+                endItr++;
+        }
+        return room;
+    }
+}
