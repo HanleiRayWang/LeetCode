78d2e6c261b00631880fbf14441927622e8d8685
HanleiRayWang <raywang0703@gmail.com>
2017-11-09 13:07:23 -0800

    Create _050Pow_x_n.java

---
+++ _050Pow_x_n.java
@@ -0,0 +1,27 @@
+/*
+Implement pow(x, n).
+
+Example 1:
+Input: 2.00000, 10
+Output: 1024.00000
+
+Example 2:
+Input: 2.10000, 3
+Output: 9.26100
+
+*/
+
+//recursive
+class Solution {
+    public double myPow(double x, int n) {
+        if(n==0)
+            return 1;
+        if(n<0){
+            n=-n;
+            x=1/x;
+        }
+        
+        int half= pow(x*x, n/2);
+        return (n%2==0)? half : half*x;
+    }
+}
