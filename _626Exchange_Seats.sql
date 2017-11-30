8618bb919c73793af384f44963a091b6d16c0ac6
HanleiRayWang <raywang0703@gmail.com>
2017-11-30 12:17:54 -0800

    Create _626Exchange_Seats.sql

---
+++ _626Exchange_Seats.sql
@@ -0,0 +1,37 @@
+/*
+
+Mary is a teacher in a middle school and she has a table seat storing students' names and their corresponding seat ids.
+
+The column id is continuous increment.
+Mary wants to change seats for the adjacent students.
+Can you write a SQL query to output the result for Mary?
++---------+---------+
+|    id   | student |
++---------+---------+
+|    1    | Abbot   |
+|    2    | Doris   |
+|    3    | Emerson |
+|    4    | Green   |
+|    5    | Jeames  |
++---------+---------+
+For the sample input, the output is:
++---------+---------+
+|    id   | student |
++---------+---------+
+|    1    | Doris   |
+|    2    | Abbot   |
+|    3    | Green   |
+|    4    | Emerson |
+|    5    | Jeames  |
++---------+---------+
+Note:
+If the number of students is odd, there is no need to change the last one's seat.
+
+*/
+
+select a.id, b.student
+from seat a, seat b
+where (a.id%2=1 and b.id=a.id+1) 
+    or (a.id%2=0 and b.id=a.id-1)
+    or (a.id%2=1 and a.id=(select count(*) from seat) and a.id=b.id)
+order by a.id
