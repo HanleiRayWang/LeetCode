881b21f772f904dabdfbff3ec2a197e459aee886
HanleiRayWang <raywang0703@gmail.com>
2017-11-30 15:44:33 -0800

    Create _601Human_Traffic_of_Stadium.sql

---
+++ _601Human_Traffic_of_Stadium.sql
@@ -0,0 +1,44 @@
+/*
+
+X city built a new stadium, each day many people visit it and the stats are saved as these columns: id, date, people
+
+Please write a query to display the records which have 3 or more consecutive rows and the amount of people more than 100(inclusive).
+
+For example, the table stadium:
++------+------------+-----------+
+| id   | date       | people    |
++------+------------+-----------+
+| 1    | 2017-01-01 | 10        |
+| 2    | 2017-01-02 | 109       |
+| 3    | 2017-01-03 | 150       |
+| 4    | 2017-01-04 | 99        |
+| 5    | 2017-01-05 | 145       |
+| 6    | 2017-01-06 | 1455      |
+| 7    | 2017-01-07 | 199       |
+| 8    | 2017-01-08 | 188       |
++------+------------+-----------+
+For the sample data above, the output is:
+
++------+------------+-----------+
+| id   | date       | people    |
++------+------------+-----------+
+| 5    | 2017-01-05 | 145       |
+| 6    | 2017-01-06 | 1455      |
+| 7    | 2017-01-07 | 199       |
+| 8    | 2017-01-08 | 188       |
++------+------------+-----------+
+Note:
+Each day only have one row record, and the dates are increasing with id increasing.
+
+*/
+
+
+select distinct a.* 
+from stadium a, stadium b, stadium c
+where  a.people>=100 and 
+       b.people>=100 and 
+       c.people>=100 and
+       ((a.id+1=b.id and a.id+2=c.id) or
+       (a.id-1=b.id and a.id+1=c.id) or
+       (a.id-2=b.id and a.id-1=c.id))
+order by a.id
