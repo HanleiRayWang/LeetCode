f8109a25056871a794627162dd5e4dbc42e73378
HanleiRayWang <raywang0703@gmail.com>
2017-12-07 10:53:32 -0800

    Update _015Three_Sum.java

--- JAVA/_015Three_Sum.java
+++ JAVA/_015Three_Sum.java
@@ -16,19 +16,27 @@ A solution set is:
 
 public class Solution{
     public List<List<Integer>> threeSum(int[] nums){
+      
+        //先排序
         Arrays.sort(nums);
         List<List<Integer>> result = new ArrayList<List<Integer>>();
+      
         for(int i=0;i<nums.length;i++){
             if(i==0 || (i>0 && nums[i]!=nums[i-1])){
                 int low = i+1, high = nums.length-1;
                 while(low<high){
+                    //三个数的index分别是i，low，high
                     int sum= nums[i]+nums[low]+nums[high];
                     if(sum==0){
                         result.add(Arrays.asList(nums[i],nums[low],nums[high]));
+                        
+                        //用两个while，跳过重复的元素
                         while(low<high && nums[low]==nums[low+1])
                             low++;
                         while(low<high && nums[high-1]==nums[high])
                             high--;
+                      
+                        //单独分别各收紧两端
                         low++;
                         high--;
                     }else if(sum<0)
