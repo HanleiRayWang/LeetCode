5441f710754b476df2cdb171af40671271a68a6f
HanleiRayWang <raywang0703@gmail.com>
2017-12-08 15:09:02 -0800

    Update _221Maximal_Square.java

--- JAVA/_221Maximal_Square.java
+++ JAVA/_221Maximal_Square.java
@@ -21,7 +21,7 @@ class Solution {
         int[][] dp = new int[matrix.length+1][matrix[0].length+1];
         for(int i=matrix.length-1;i>=0;i--){
             for(int j=matrix[0].length-1;j>=0;j--){
-                if(matrix[i][j]!='0'){
+                if(matrix[i][j]=='1'){
                     dp[i][j]+=1;
                     if(dp[i+1][j+1]!=0 && dp[i+1][j]!=0 && dp[i][j+1]!=0)
                         dp[i][j]+=Math.min(dp[i+1][j+1], Math.min(dp[i+1][j], dp[i][j+1]));
