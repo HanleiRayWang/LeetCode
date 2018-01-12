be882f70ca4b457711d664eec4eef2735d5c75a8
HanleiRayWang <raywang0703@gmail.com>
2018-01-11 19:07:50 -0800

    Update _026Remove_Duplicates_from_Sorted_Array.java

--- JAVA/_026Remove_Duplicates_from_Sorted_Array.java
+++ JAVA/_026Remove_Duplicates_from_Sorted_Array.java
@@ -21,10 +21,9 @@ class Solution {
             return nums.length;
         
         int count=1;
-        for(int i=1; i<nums.length; i++){
+        for(int i=1; i<nums.length; i++)
             if(nums[i-1]!=nums[i])
                 nums[count++]=nums[i];
-        }
 
         return count;
     }
